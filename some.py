05e5ec61640f204d41dadb3796842cd96a06d8f3
talatmursalin <tanveer.2387@gmail.com>
2021-03-04 16:31:09 +0600

    initial commit

---
+++ some.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2021 mursalin <mail@mail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+prints something                         
+"""
+
+print("something")
